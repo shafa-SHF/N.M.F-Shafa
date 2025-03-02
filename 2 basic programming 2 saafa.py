f2b1da3b5c3632d702bb6826e966903a2de50ed6
shafa-SHF <naina.mohammed_ti24@nusaputra.ac.id>
2025-03-02 22:04:22 +0700

    Add files via upload

---
+++ 2 basic programming 2 saafa.py
@@ -0,0 +1,9 @@
+x="Nusa putra university"
+y=2002
+z=743.67
+y=int(y)
+z=float(z)
+if isinstance(x,str) and isinstance(y,int) and isinstance(z,float):
+    print ("valid input types")
+else:
+    print ("invalid input types")
---
+++ 2 basic programming 3 saafa.py
@@ -0,0 +1,13 @@
+X=input("enter the X:")
+Y=input("enter the Y:")
+
+X=bool(X)
+Y=bool(Y)
+
+print ("RESULTS: ")
+print ("X and Y:", X and Y)
+print ("X or Y:", X or Y)
+print ("not X:",not X)
+print (" X xor Y:", X!= Y)
+
+  
